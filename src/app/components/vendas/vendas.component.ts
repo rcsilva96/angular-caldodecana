b712addfd6c69ee1890c22c8aa096de9166d938f
Renato <rsilv1822@gmail.com>
2024-02-06 23:51:56 -0300

    Projeto base

---
+++ src/app/components/vendas/vendas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendas',
+  standalone: true,
+  imports: [],
+  templateUrl: './vendas.component.html',
+  styleUrl: './vendas.component.css'
+})
+export class VendasComponent {
+
+}
